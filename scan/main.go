c76f16f6f2f54a15ec251072ea01920814d940da
ebert <shengzhe.liao@mirattery.com>
2023-03-09 19:59:21 +0800

    并行操作和管道操作入门

--- scan/main.go
+++ scan/main.go
@@ -3,13 +3,33 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
+	"time"
 )
 
+func main() {
+	var wg sync.WaitGroup
+	for i := 1; i <= 81; i++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			address := fmt.Sprintf("130.162.128.154:%d", j)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("%d open\n", j)
+		}(i)
+	}
+	wg.Wait()
+}
+
 // Scan0 笨笨的单端口扫描
-func Scan0() {
-	_, err := net.Dial("tcp", "www.eber.vip:80")
+func Scan0(protocol string, address string) {
+	_, err := net.Dial(protocol, address)
 	if err == nil {
-		fmt.Printf("Connection to www.eber.vip port :80 [tcp] succeeded! ")
+		fmt.Printf("Connection to %s [%s] succeeded! ", protocol, address)
 	} else {
 		fmt.Println(err)
 	}
@@ -17,7 +37,8 @@ func Scan0() {
 
 // Scan1 笨笨的多端口for循环扫描 从1-443端口
 func Scan1() {
-	for i := 1; i < 443; i++ {
+	fmt.Println(time.Now())
+	for i := 1; i <= 443; i++ {
 		dial, err := net.Dial("tcp", fmt.Sprintf("www.eber.vip:%d", i))
 		if err == nil {
 			// 监听到端口open后  关闭连接
@@ -25,5 +46,28 @@ func Scan1() {
 			fmt.Printf("Discovered open port %d/tcp on www.eber.vip \n", i)
 		}
 	}
+	fmt.Println(time.Now())
+}
 
+// Scan2 错误的协程示范
+func Scan2() {
+	fmt.Println(time.Now())
+	for i := 1; i <= 443; i++ {
+		go func(j int) {
+			dial, err := net.Dial("tcp", fmt.Sprintf("www.eber.vip:%d", j))
+			if err == nil {
+				// 监听到端口open后  关闭连接
+				err = dial.Close()
+				fmt.Printf("Discovered open port %d/tcp on www.eber.vip \n", j)
+			}
+		}(i)
+	}
+	fmt.Println(time.Now())
+}
+
+func worker(ports chan int, wg *sync.WaitGroup) {
+	for port := range ports {
+		fmt.Println(port)
+		wg.Done()
+	}
 }
--- scan/scan_test.go
+++ scan/scan_test.go
@@ -1,10 +1,58 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"net"
+	"sync"
+	"testing"
+)
 
 func TestScan0(t *testing.T) {
-	Scan0()
+	Scan0("tcp", "130.162.128.154:80")
 }
 func TestScan1(t *testing.T) {
 	Scan1()
+}
+func TestScan2(t *testing.T) {
+	Scan2()
+}
+
+// 后面都用test写，方便运行
+
+// goroutine并行操作
+func TestScan3(t *testing.T) {
+	var wg sync.WaitGroup
+	for i := 1; i <= 81; i++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			address := fmt.Sprintf("130.162.128.154:%d", j)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("%d open\n", j)
+		}(i)
+	}
+	wg.Wait()
+}
+
+func TestScan4(t *testing.T) {
+	ports := make(chan int, 10)
+	var wg sync.WaitGroup
+	// 获取非空长度
+	fmt.Println(len(ports))
+	// 管道的左指针到末尾的空间容量
+	fmt.Println(cap(ports))
+	println("---------------")
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, &wg)
+	}
+	for i := 1; i <= 100; i++ {
+		wg.Add(1)
+		ports <- i
+	}
+	wg.Wait()
+	close(ports)
 }
